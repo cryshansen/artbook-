ab900ad2a415f2e39286071954da893dbed469a3
Crystal Hansen <59895987+cryshansen@users.noreply.github.com>
2020-05-06 17:44:59 -0600

    Create authority.cs

---
+++ artbook/authority.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using System.Transactions.TransactionScope.TransactionScope;
+namespace NaitBook
+{
+    internal class authority
+    {
+//      
+
+   
+   //public static void AddAlbumImage(int UserId, string AlbumName, string location, string albumDesc, string privacy)
+   //{
+        //     using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required, options)) {
+   //            int AlbumId = Users.AddUserAlbums(UserId, AlbumName,  location,  albumDesc,  privacy);
+   //             Users.AddUserAlbumPhotos(AlbumId, "Question.jpg", "No Caption", 1);            
+   //             scope.Complete();
+   //             // AddUserAlbumPhotos(AlbumID, "Question.jpg", "No Caption Available", 1);
+   //         }
+   //}
+
+
+
+
+
+    }
+}
